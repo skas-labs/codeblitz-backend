1a47de18aa6b2b5e0d62105899e63ba2f1a62709
Arnav Gupta <arnav@codingblocks.com>
2021-01-08 17:20:10 +0530

    better api models
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- api/src/players/players/players.controller.ts
+++ api/src/players/players/players.controller.ts
@@ -1,23 +1,27 @@
 import { Controller, Get, Inject, Param, Query } from '@nestjs/common';
 import { PlayersService } from '../../database/players/players.service';
 import { Player } from '@codeblitz/data/dist/entities/Player';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiPropertyOptional, ApiTags } from '@nestjs/swagger';
+
+class FindAllQueryParams {
+  @ApiPropertyOptional() username?: string;
+  @ApiPropertyOptional() name?: string;
+}
 
 @ApiTags('players')
 @Controller('api/players')
 export class PlayersController {
-  @Inject() private readonly players!: PlayersService
+  @Inject() private readonly players!: PlayersService;
 
   @Get()
   async findAll(
-    @Query('username') username?: string,
-    @Query('name') name?: string
+    @Query() queries: FindAllQueryParams
   ): Promise<Player[]> {
-    return await this.players.search({ username, name })
+    return await this.players.search(queries);
   }
 
   @Get(':id')
   async findById(@Param('id') id: number): Promise<Player> {
-    return await this.players.findById(id)
+    return await this.players.findById(id);
   }
 }
